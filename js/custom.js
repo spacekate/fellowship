934a276ed6745f11dcfa5b888d266c3a379684fe
Kate Currie <spacekate@gmail.com>
2018-07-07 16:12:20 +1000

    making the fellow cards look pretty

--- js/custom.js
+++ js/custom.js
@@ -9,19 +9,33 @@
 $(document).ready(function() {
     $.fn.extend( {
         appendFellow: function (fellowData) {
-            var newFellow = $("<div>").attr("id", fellowData["id"]).addClass("col-md-6 col-lg-4 align-top");
-            var fellowBody = $("<div>").addClass("fellow");
-            var name = $("<p>").text(fellowData['name']['first'] + " " + fellowData['name']['last']);
-            var fellowship = $("<p>").text(fellowData['fellowship']);
-            var age = $("<p>").text("age: " + fellowData['age']);
-            var email = $("<a>").text(fellowData['email']).attr("href", "mailto:" + fellowData['email']);
-            var about = $("<p>").text(fellowData['about']);
-            alert(fellowData['isActive']);
-            if (fellowData['isActive'] == "false") {
+            var newFellow = $("<div>")
+                .attr("id", fellowData["id"])
+                .addClass("col-md-6 col-lg-4 align-top");
+            var fellowBody = $("<div>")
+                .addClass("fellow");
+            var name = $("<p>")
+                .text(fellowData['name']['first'] + " " + fellowData['name']['last']);
+            var fellowship = $("<p>")
+                .text(fellowData['fellowship']);
+            var age = $("<p>")
+                .text("age: " + fellowData['age']);
+            var email = $("<a>")
+                .text(fellowData['email'])
+                .attr("href", "mailto:" + fellowData['email']);
+            var about = $("<p>")
+                .text(fellowData['about']);
+
+            if (fellowData['isActive'] && fellowData['isActive'] == "false") {
                 $(newFellow).addClass("inactive");
             }
             $(newFellow).append(fellowBody);
-            $(fellowBody).append(name).append(fellowship).append(age).append(email).append(isActive).append(about);
+            $(fellowBody)
+                .append(name)
+                .append(fellowship)
+                .append(age).append(email)
+                .append(isActive)
+                .append(about);
             $(this).append(newFellow);
         }
     });
