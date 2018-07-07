f53149fc80e0180f77d979e15fa70949d8e51bb9
Kate Currie <spacekate@gmail.com>
2018-07-07 13:38:51 +1000

    displaying json

--- js/custom.js
+++ js/custom.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
         var fellow;
 
         $.each(data, function(key, value) {
-            alert("key = " + key + " /nvalue = " + value);
+            alert("key = " + key + " \nvalue = " + value);
             fellow = jQuery.parseJSON(value);
             console.log(fellow);
             //$("#fellows").append(fellow.id);
