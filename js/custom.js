9a767871f49695cbd86b1b902c907279ba23afc9
Kate Currie <spacekate@gmail.com>
2018-07-07 14:27:00 +1000

    displaying json

--- js/custom.js
+++ js/custom.js
@@ -10,14 +10,8 @@ $(document).ready(function() {
     $.getJSON("/fellowship/fellows_dataset.json", function(data){
 
         $.each(data, function() {
-            var $fellow = $("<div>", {id: this['id']});
-            var $activityIndicator = $("<p>", {innerHTML: this['isActive']});
-            var $age = $("<span>", {innerHTML: this['age']});
 
-            $fellow.append($activityIndicator);
-            $fellow.append($age);
-
-            $("#fellows").append($fellow);
+            $("#fellows").append($("<span>", {innerHTML: this['age']}));
         });
     });
 });
