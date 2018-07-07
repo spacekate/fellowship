4e15defb0cd6ed6de33b12f9cf9fa1993a10f35d
Kate Currie <spacekate@gmail.com>
2018-07-07 19:39:33 +1000

    all done!

--- js/custom.js
+++ js/custom.js
@@ -7,6 +7,7 @@
  */
 
 $(document).ready(function() {
+    var fellowship_data;
     $.fn.extend( {
         appendFellow: function (fellowData) {
             var newFellow = $("<div>")
@@ -14,25 +15,31 @@ $(document).ready(function() {
                 .addClass("col-lg-10 offset-lg-1");
             var fellowBody = $("<div>")
                 .addClass("fellow");
-            var name = $("<p>")
+            var name = $("<h3>")
                 .text(fellowData['name']['first'] + " " + fellowData['name']['last']);
             var fellowship = $("<p>")
-                .text(fellowData['fellowship']);
+                .text("Fellowship: " + fellowData['fellowship']);
             var age = $("<p>")
                 .text("age: " + fellowData['age']);
+            var about = $("<p>")
+                .text(fellowData['about']);
             var email = $("<a>")
                 .text(fellowData['email'])
                 .attr("href", "mailto:" + fellowData['email']);
-            var about = $("<p>")
-                .text(fellowData['about']);
+            var registered = $("<span>")
+                .text("Registered: " + fellowData['registered'])
+                .addClass("small");
             var isActive = $("<span>")
-                .addClass("status");
+                .addClass("status small");
+            var idNumber = $("<span>")
+                .addClass("small idNumber")
+                .text("Id: " + fellowData['id']);
 
             if (!fellowData['isActive']) {
                 $(newFellow).addClass("text-muted");
                 isActive.text("inactive");
             } else {
-                isActive.text("active");
+                isActive.text("active").addClass("active");
             }
 
             $(newFellow).append(fellowBody);
@@ -42,15 +49,85 @@ $(document).ready(function() {
                 .append(age)
                 .append(about)
                 .append($("<p>").text("email: ").append(email))
+                .append(registered)
+                .append(idNumber)
                 .append(isActive);
             $(this).append(newFellow);
         }
     });
 
-    $.getJSON("/fellowship/fellows_dataset.json", function(data){
+    $.getJSON("../fellows_dataset.json", function(data){
+        fellowship_data = data;
+        refreshFellows(fellowship_data);
+    });
 
+    $("#search-button").click(function() {
+        var filteredData = new Array();
+        var searchTerm = $("#searchFor").val().toLowerCase();
+
+
+        $.each(fellowship_data, function() {
+            if ((this["about"].toLowerCase().indexOf(searchTerm) != -1)
+                || (this["name"]["first"].toLowerCase().indexOf(searchTerm) != -1)
+                || (this["name"]["last"].toLowerCase().indexOf(searchTerm) != -1)
+                || (this["fellowship"].toLowerCase().indexOf(searchTerm) != -1)) {
+                filteredData.push(this);
+            }
+        });
+
+        refreshFellows(filteredData);
+    });
+
+    $("#filter-button").click(function() {
+        var filterType = ($("#filterBy option:selected").val());
+        // clear search box to minimize confustion
+        $("#searchFor").val("");
+
+        switch (filterType) {
+            case "fellowship":
+                // order by fellowship
+                fellowship_data.sort(fellowshipSort);
+                refreshFellows(fellowship_data);
+                break;
+            case "active":
+                // order by active/inactive
+                fellowship_data.sort(activeSort);
+                refreshFellows(fellowship_data);
+                break;
+            default:
+                // order alphabetically
+                fellowship_data.sort(lastNameSort);
+                refreshFellows(fellowship_data);
+        }
+    });
+
+    function refreshFellows (data) {
+        $("#fellows").empty();
         $.each(data, function() {
             $("#fellows").appendFellow(this);
         });
-    });
+    }
+
+
+    function fellowshipSort(a, b) {
+        return attributeSort(a["fellowship"], b["fellowship"]);
+    }
+
+    function activeSort(a, b) {
+        return attributeSort(b["isActive"], a["isActive"]);
+    }
+
+    function lastNameSort(a, b) {
+        return attributeSort(a["name"]["last"], b["name"]["last"]);
+    }
+
+    function attributeSort(aName, bName) {
+        if (aName < bName) {
+            return -1;
+        } else if (aName > bName) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 });
