4dd601e56f9876cf5a8cde925399b4181de8af45
Kate Currie <spacekate@gmail.com>
2018-07-07 12:25:27 +1000

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+/* Custom JavaScript
+ *
+ * Author: Kate Currie
+ * Created: 7 July 2018
+ *
+ * Description: Read and display a json dataset. Add filters
+ */
+
+$(document).ready(function() {
+    $.getJSON("../fellows_dataset.json", function(result){
+        $.each(result, function(i, field){
+            console.log(data);
+        });
+    });
+});
