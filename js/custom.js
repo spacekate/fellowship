397ec8474193ce5cd295158c2913d7ab95470e8d
Kate Currie <spacekate@gmail.com>
2018-07-07 13:41:21 +1000

    displaying json

--- js/custom.js
+++ js/custom.js
@@ -12,7 +12,6 @@ $(document).ready(function() {
 
         $.each(data, function(key, value) {
             alert("key = " + key + " \nvalue = " + value);
-            fellow = jQuery.parseJSON(value);
             $("#fellows").append(fellow.id);
         });
     });
